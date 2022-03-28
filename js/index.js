530dbbc9b6c285e09b5a3aa6c2bb38b17a3d18c9
fesolo2122daw2 <fesolo2122daw2@protonmail.com>
2022-03-28 16:58:49 +0200

    Creació taulell i imputs

---
+++ js/index.js
@@ -0,0 +1,89 @@
+window.addEventListener('load', carga);
+
+function carga(){
+    
+    let n=0;
+
+    for (let i = 0; i < 9; i++) {
+        fila = document.createElement('tr');
+        fila.setAttribute("id", i);
+        document.getElementById('taulell').appendChild(fila);
+        for (let j = 0; j < 9; j++) {
+            cella = document.createElement('td');
+            cella.setAttribute("id", i + '-' + j);
+            document.getElementById(i).appendChild(cella);
+            numero = document.createElement('input');
+            numero.setAttribute("id", 'numero'+n);
+            numero.setAttribute('maxlength', '1');
+            document.getElementById(i + '-' + j).appendChild(numero);
+            n++;
+        }
+    }
+    
+
+    /*class sudoku1{
+        constructor(resolt,noresolt){
+            this.resolt = [[1,6,5,3,9,7,4,7,8][3,2,4,1,8,7,5,6,9][7,8,9,2,6,5,3,4,1][9,4,6,8,1,3,7,5,2][8,5,3,7,2,9,4,1,6][2,1,7,5,4,6,8,9,3][6,7,1,4,3,2,9,8,5][5,3,8,9,7,1,6,2,4][4,9,2,6,5,8,1,3,7]];
+            this.noresolt = [[1,6,5,3,0,0,0,7,8][3,2,0,1,0,0,5,0,0][0,0,0,2,6,0,0,4,0][9,0,6,0,0,3,0,0,0][0,0,0,7,2,9,0,0,6][0,0,7,0,4,0,8,0,0][0,0,0,0,0,0,9,0,0][0,3,8,0,0,0,0,0,0][0,0,0,6,0,0,1,0,7]];
+        }
+    }
+
+    class sudoku2{
+        constructor(resolt,noresolt){
+            this.resolt = [[7,3,1,5,6,9,4,2,8][5,2,9,8,1,4,7,3,6][6,4,8,3,2,7,5,1,9][2,8,4,1,7,6,9,5,3][1,9,5,4,3,8,6,7,2][3,6,7,2,9,5,8,4,1][4,1,6,7,8,3,2,9,5][8,5,3,9,4,2,1,6,7][9,7,2,6,5,1,3,8,4]];
+            this.noresolt = [[7,0,1,0,0,9,0,2,0][0,0,9,0,0,0,0,0,6][0,4,0,0,2,0,5,1,0][0,0,0,0,0,6,9,0,3][0,0,5,0,0,8,0,0,0][0,0,0,0,9,0,8,4,1][0,0,6,7,0,3,0,0,0][8,5,0,0,0,0,0,0,0][0,0,0,0,5,0,3,0,0]];
+        }
+    }
+
+    class sudoku3{
+        constructor(resolt,noresolt){
+            this.resolt = [[6,7,5,8,1,3,9,4,2][3,9,1,4,7,2,8,5,6][8,4,2,6,5,9,1,7,3][1,6,8,9,4,7,2,3,5][9,2,3,5,8,6,4,1,7][4,5,7,2,3,1,6,9,8][2,1,4,3,6,5,7,8,9][5,8,9,7,2,4,3,6,1][7,3,6,1,9,8,5,2,4]];
+            this.noresolt = [[0,0,5,0,0,0,0,4,0][0,0,0,4,7,0,0,0,6][8,0,2,0,0,0,1,0,0][1,0,0,0,0,0,2,0,0][0,0,3,0,0,6,0,0,0][0,5,7,0,3,0,0,0,0][0,1,0,0,6,5,0,8,0][0,0,0,0,0,0,3,0,0][0,0,0,1,9,0,5,0,0]];
+        }
+    }*/
+
+    class sudoku{
+        constructor(resolt,noresolt){
+            this.resolt;
+            this.noresolt;
+        }
+    }
+
+
+    let app = new Vue({
+        el: '#app',
+        data: {
+          lazy:'',
+          number:0,
+          trim:'',
+          noTrim:''
+        },
+        template: `
+    <section>
+        <h4>Lazy</h4>
+  <input v-model.lazy="lazy" type="text" />
+  <p>
+      <strong>Lazy:</strong> {{lazy}}
+  </p>
+  <h4>Number</h4>
+  <input v-model.number="number" type="text" />
+  <p>
+      <strong>Number:</strong> {{number}}
+  </p>
+  <p>
+      <strong>Type of Number:</strong> {{typeof number}}
+  </p>
+  <h4>No Trim</h4>
+  <input v-model="noTrim" type="text" />
+  <p>
+      <strong>No Trim:</strong> "{{noTrim}}"
+  </p>
+  <h4>Trim</h4>
+  <input v-model.trim="trim" type="text" />
+  <p>
+      <strong>Trim:</strong> "{{trim}}"
+  </p>
+  </section>
+        `
+      })
+}
