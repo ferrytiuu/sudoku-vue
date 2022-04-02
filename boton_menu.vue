87f05187ad36dbdd2058fd5de88c1994341726aa
dafepe <15585360.clot@fje.edu>
2022-04-02 18:19:48 +0200

    añadido componente

---
+++ boton_menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button v-on:click="clicked('you just clicked on button 2')">
+      Button 2
+    </button>
+    <h1>{{text2}}</h1>
+  </div>
+</template>;
+<script>
+export default {
+  name: "Modal",
+  props:['text2']
+};
+</script>
---
+++ indexdb.js
@@ -0,0 +1,65 @@
+const dbName = "sudokuNames";
+
+let db;
+
+
+let accesDB = new Promise(function (myResolve, myReject) {
+    var request = indexedDB.open(dbName, 8);
+
+    request.onerror = function (event) {
+        myReject();
+    };
+    request.onsuccess = function (event) {
+        db = event.target.result;
+        myResolve();
+    };
+    request.onupgradeneeded = function (event) {
+        var db = event.target.result;
+        try {
+            db.deleteObjectStore("titols");
+        } catch (e) {
+
+        }
+        db = event.target.result;
+        var objectStore = db.createObjectStore("titols", {keyPath: "id"});
+
+        objectStore.createIndex("id", "id", {unique: true});
+
+        objectStore.transaction.oncomplete = function (event) {
+            var customerObjectStore = db.transaction("titols", "readwrite").objectStore("titols");
+            customerData = [
+                {id: 1, nom: "sudoku12"},
+                {id: 2, nom: "sudoku22"},
+                {id: 3, nom: "sudoku32"},
+            ]
+            for (var i in customerData) {
+                customerObjectStore.add(customerData[i]);
+            }
+            myResolve();
+
+        }
+
+    };
+});
+
+async function obtenerTitulo(id) {
+    var customerObjectStore = db.transaction("titols", "readwrite").objectStore("titols");
+    console.log("13");
+    let entrades=[];
+    return new Promise((resolve, reject) => {
+        customerObjectStore.openCursor().onsuccess = function (event) {
+            var cursor = event.target.result;
+            if (cursor) {
+                console.log("Name for SSN " + cursor.key + " is " + cursor.value.nom);
+                entrades.push(cursor.value.nom);
+                cursor.continue();
+            } else {
+                console.log("No more entries!");
+                resolve(entrades);
+            }
+        };
+
+
+    })
+
+}
